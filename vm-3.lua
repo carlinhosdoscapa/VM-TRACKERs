54a0710236057c6cf8e557f707e60b418be3d508
carlinhosdoscapa <carlinhosnomatagal208@gmail.com>
2024-09-24 02:02:08 -0300

    Update vm-3.lua

--- vm-3.lua
+++ vm-3.lua
@@ -1,1 +1,38 @@
 
+_G.Settings_Farm = {
+    ["Start_Farm"] = true,
+    ["Enabled_HOP"] = true,
+    ["Fruits_List"] = {
+        'RumbleRumble',
+        'FlameFlame',
+        'MagmaMagma',
+        'LightLight',
+        'GasGas',
+        'IceIce',
+        'SnowSnow',
+        'QuakeQuake',
+        'SpiritSpirit',
+        'PhoenixPhoenix',
+        'DoughDough',
+        'DragonDragon'
+    },
+    ["Enabled_Invisible"] = true,
+    ["Enabled_FastFarm"] = true,
+    ["Amount_FPS"] = 30,
+    ["LOCK_FPS"] = false,
+    ["WhiteScreen"] = true,
+    ["Disabled_Gui"] = true,
+}
+_G.Quests_Settings = {
+     ["V2Observation"] = false,
+     ["V2Aramament"] = false,
+     ["V2DarkLeg"] = false,
+     ["FarmRaid"] = false
+}
+-- Script Here!!
+
+
+getgenv().Key = "MMARU-CQ4OC-5XUTJ-HW6T-TO7FF-XYFP5"
+getgenv().id = "1037147029614362624"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
